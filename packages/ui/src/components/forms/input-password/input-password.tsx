eff8b5f4accbc1ea25b773e49b6d13b4bb5fe83f
Adam Fratino <fratino.a@gmail.com>
2025-10-14 01:11:17 -0400

    refactor: input-password after slot

--- packages/ui/src/components/forms/input-password/input-password.tsx
+++ packages/ui/src/components/forms/input-password/input-password.tsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 import { Input } from "../input/input";
 
 import type { InputPasswordProps } from "./input-password.types";
-import "./input-password.styles.css";
 
 const ICON_SIZE = 12;
 
@@ -20,17 +19,9 @@ export const InputPassword = ({ ...props }: InputPasswordProps) => {
       {...props}
       type={showPassword ? "text" : "password"}
       after={
-        <button
-          data-slot="password-visibility-toggle"
-          onClick={handleTogglePassword}
-        >
-          {showPassword ? (
-            <Eye size={ICON_SIZE} />
-          ) : (
-            <EyeOff size={ICON_SIZE} />
-          )}
-        </button>
+        showPassword ? <Eye size={ICON_SIZE} /> : <EyeOff size={ICON_SIZE} />
       }
+      afterOnClick={handleTogglePassword}
     />
   );
 };
