b37b11c32933e11bac4c41faf652d5bafaf9b5d6
Adam Fratino <fratino.a@gmail.com>
2025-12-13 02:20:29 -0500

    feat: add droppable-cell

---
+++ packages/calendars/src/event-calendar/subcomponents/droppable-cell.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useDroppable, type UniqueIdentifier } from "@dnd-kit/core";
+
+import { Stack, type StackProps } from "@uiid/layout";
+import { cx } from "@uiid/utils";
+
+import { useEventCalendarDnd } from "../event-calendar.context";
+import type { DropTargetData } from "../event-calendar.types";
+
+import styles from "./droppable-cell.module.css";
+
+type DroppableCellProps = DropTargetData &
+  Omit<StackProps, "id"> & {
+    id: UniqueIdentifier;
+  };
+
+export const DroppableCell = ({
+  id,
+  date,
+  time,
+  className,
+  children,
+  ...props
+}: DroppableCellProps) => {
+  const { activeEvent } = useEventCalendarDnd();
+
+  const { setNodeRef, isOver } = useDroppable({
+    id,
+    data: {
+      date,
+      time,
+    },
+  });
+
+  return (
+    <Stack
+      ref={setNodeRef}
+      data-dragging={isOver && activeEvent ? true : undefined}
+      className={cx(styles["droppable-cell"], className)}
+      fullheight
+      py={1}
+      {...props}
+    >
+      {children}
+    </Stack>
+  );
+};
+DroppableCell.displayName = "DroppableCell";
