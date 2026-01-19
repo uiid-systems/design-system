2c9c52c18ac7940ca8f17b2cca9d1c75309a938c
Adam Fratino <fratino.a@gmail.com>
2026-01-19 01:22:01 -0500

    refactor: consolidate table styles

--- packages/tables/src/table/subcomponents/table-root.tsx
+++ packages/tables/src/table/subcomponents/table-root.tsx
@@ -2,7 +2,7 @@ import { cx } from "@uiid/utils";
 
 import type { TableRootProps } from "../table.types";
 
-import styles from "./table-root.module.css";
+import styles from "../table.module.css";
 
 export const TableRoot = ({
   striped,
--- packages/tables/src/table/subcomponents/table-row.tsx
+++ packages/tables/src/table/subcomponents/table-row.tsx
@@ -1,7 +1,7 @@
 import { Box, type BoxProps } from "@uiid/layout";
 import { cx } from "@uiid/utils";
 
-import styles from "./table-row.module.css";
+import styles from "../table.module.css";
 
 export const TableRow = ({
   className,
